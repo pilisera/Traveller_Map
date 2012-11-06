45be64ea573e0956dd44b853f1d6dab2f0635bc2
Andrew Piliser <pilisera@gmail.com>
2012-11-05 21:44:13 -0500

    Added Haml and CSS
    Made the code a lot nicer by using Haml and CSS instead of raw HTML.
    Unfortunately the images don't line up quite right, so I'm going to look
    at the API for a better way to give the jump map context.

--- Traveller.rb
+++ Traveller.rb
@@ -2,36 +2,24 @@ require 'sinatra'
 require 'json'
 require 'net/http'
 require 'uri'
+require 'haml'
 
-#use p not puts
-
-currentLocation = 1, 1, 23, 38
+current_location = 1, 1, 23, 38
 range = 2
 
 get '/' do
-	worlds = jumpWorlds(currentLocation, range)
+	worlds = jump_worlds(current_location, range)
 
 	worlds = JSON.parse(worlds)["Worlds"]
-
-	worldHtml = ""
 	
-	worlds.each do |world|
-		name = world["Name"]
-		uwp = world["UWP"]
-
-		worldHtml = worldHtml + "</br>" + "Name: " + name + ", UWP: " + uwp
-	end
-	
-	
-	jumpMap(currentLocation, 2) + "</br>" + worldHtml + "</br></br>" + "<a href=\"/widemap\">Click Here for a wider view</a>"
-	
+	haml :main, :locals => { :img_url => jump_map(current_location, 2), :img_2_url => jump_map(current_location, 5), :world_data => worlds }	
 end
 
 get '/widemap/?' do 
-	jumpMap(currentLocation, 8)
+	jump_map(current_location, 8)
 end
 
-def jumpMap(location, n)
+def jump_map(location, n)
 	# Uses the JumpMap part of the API http://www.travellermap.com/api.htm#jumpmap
 	# location = array of sx, sy, hx, hy coords http://www.travellermap.com/api.htm#system_sectorhex
 	# n = jump distance
@@ -47,11 +35,9 @@ def jumpMap(location, n)
 	restPart = "scale=48"
 
 	url = url + sxPart + syPart + hxPart + hyPart + jumpPart + restPart
-
-	"<img src=#{url}/>"
 end
 
-def jumpWorlds(location, n)
+def jump_worlds(location, n)
 	sx, sy, hx, hy = location
 
 	url = 'http://www.travellermap.com/JumpWorlds.aspx?'
